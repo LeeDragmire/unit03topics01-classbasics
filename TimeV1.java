0e6f8b9cc89dfd245f1d9713975780d000c7b76c
colton <adamscolton64@gmail.com>
2023-03-01 12:03:33 -0600

    TimeV1 almost done

--- TimeV1.java
+++ TimeV1.java
@@ -1,4 +1,31 @@
 public class TimeV1 {
+    private int hours;
+    private int minutes;
+    private int seconds;
+    private boolean am;
+
+    public TimeV1(int curHours, int curMinutes, int curSeconds, boolean isAM) {
+        hours = curHours;
+        minutes = curMinutes;
+        seconds = curSeconds;
+        am = isAM;
+    }
+    public int getHours() {
+        return hours;
+    }
+    public int getMinutes() {
+        return minutes;
+    }
+    public int getSeconds() {
+        return seconds;
+    }
+    public String toString() {
+        String output = "";
+        if (am) {
+
+        }
+        return output;
+    }
     public static void main(String[] args) {
         
     }
