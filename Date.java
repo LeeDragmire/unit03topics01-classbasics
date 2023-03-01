66a2a1155906d9b64443f4acff8b8ee84f697c28
colton <adamscolton64@gmail.com>
2023-03-01 12:03:18 -0600

    Date complete

--- Date.java
+++ Date.java
@@ -1,5 +1,41 @@
 public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int curDay, int curMonth, int curYear) {
+        day = curDay;
+        month = curMonth;
+        year = curYear;
+    }
+
+    public int getDay() {
+        return day;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getYear() {
+        return year;
+    }
+
+    public String toString() {
+        String output = "";
+        if (getDay() < 10) {
+            output += "0" + getDay() + "/";
+        } else{
+            output += getDay() + "/";
+        }
+        if (getMonth() < 10) {
+            output += "0" + getMonth() + "/";
+        } else{
+            output += getMonth() + "/";
+        }
+        output += getYear();
+        return output;
+    }
     public static void main(String[] args) {
-        
+        Date test = new Date(1, 3, 2023);
+        System.out.println(test);
     }
 }
